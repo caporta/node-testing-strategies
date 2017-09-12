aaa50123417fb240e7b28b30ad7b3ad09102a77c
Chris Aporta <christopher.aporta@gmail.com>
2017-09-12 00:12:35 -0400

    Add bad path tests and remove happy path

--- test/mission_application_spec.js
+++ test/mission_application_spec.js
@@ -18,28 +18,40 @@ describe('Applying for mission', () => {
   })
 
   describe('Application valid if...', () => {
-    it('has an email is greater than four characters and contains a @', () => {
-      assert(validApp.emailIsValid(), 'email is invalid')
-    })
-
-    it('has a height is between 60 and 75', () => {
-      assert(validApp.heightIsValid(), 'height is invalid')
-    })
-
-    it('has a age is between 15 and 100', () => {
-      assert(validApp.weightIsValid(), 'weight is invalid')
-    })
-
-    it('has a weight is between 100 and 300', () => {
-      assert(validApp.weightIsValid(), 'age is invalid')
-    })
-
-    it('has a first and last name', () => {
-      assert(validApp.nameIsValid(), 'name is invalid')
-    })
-
     it('has successful validators', () => {
       assert(validApp.isValid(), 'application is invalid')
     });
+  })
+
+  describe('Application invalid if...', () => {
+    it('has an email four characters or less', () => {
+      const app = new MembershipApplication({ email: 'dd' })
+      assert(!app.emailIsValid())
+    })
+
+    it('has an email without a @', () => {
+      const app = new MembershipApplication({ email: 'dd' })
+      assert(!app.emailIsValid())
+    })
+
+    it('has an omitted email', () => {
+      const app = new MembershipApplication()
+      assert(!app.emailIsValid())
+    })
+
+    it('has a height less than 60 inches', () => {
+      const app = new MembershipApplication({ height: 50 })
+      assert(!app.heightIsValid())
+    })
+
+    it('has a height more than 75 inches', () => {
+      const app = new MembershipApplication({ height: 80 })
+      assert(!app.heightIsValid())
+    })
+
+    it('has an omitted height', () => {
+      const app = new MembershipApplication()
+      assert(!app.heightIsValid())
+    })
   })
 })
